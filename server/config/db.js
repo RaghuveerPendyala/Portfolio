dfcb3075f7d613087ef15cc381c20f08f685a48f
Raghuveer Pendyala <raghuveerkumar126@gmail.com>
2022-02-19 22:57:38 +0530

    assignment 2

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/business_store"
+    //"URI": "mongodb://localhost/business_store"
+    "URI": "mongodb+srv://raghuveer:raghu126@cluster0.7cnx2.mongodb.net/business_list?retryWrites=true&w=majority"
 }
 module.exports.Secret = "SomeSecret";
